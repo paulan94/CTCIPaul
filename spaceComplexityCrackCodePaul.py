7952c4d8011624825d37b20626635ae94d46d645
paulan <paulan94@gmail.com>
2017-06-22 14:06:46 -0700

    added spacetimecomplexity in python

---
+++ spaceComplexityCrackCodePaul.py
@@ -0,0 +1,11 @@
+def pairSumSeq(n):
+    ans_sum = 0;
+    for i in range(n):
+        ans_sum += pairSum(i,i+1)
+    return ans_sum
+
+def pairSum(a, b):
+    return a+b
+
+if __name__ == "__main__":
+    print (pairSumSeq(2))
